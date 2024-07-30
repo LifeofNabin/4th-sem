0bd586d5429b935b13c96b2164b022b3ffcc5e55
Nabin <70052993+LifeofNabin@users.noreply.github.com>
2024-07-30 19:32:51 +0900

    Greedy_knapsack.c

--- ADA/Lab programs/Greedy_knapsack.c
+++ ADA/Lab programs/Greedy_knapsack.c
@@ -2,7 +2,7 @@
 int n,m,p[10],w[10];
 void greedy_knapsack()
 {
-float max, profit=0;
+float max,profit=0;
 int k=0,i,j;
 printf("item included is :");
 for(i=0;i<n;i++)
@@ -18,10 +18,10 @@ max=((float)p[j])/w[j];
 }
 }
 //kth element has highest price to weight ratio
-if(w[k] <= m )
+if(w[k]<=m )
 {
 printf("%d",k);
-m = m - w[k];
+m= m-w[k];
 profit=profit+p[k];
 p[k]=0;
 }
@@ -29,9 +29,8 @@ else
 break;//unable fit item k into knapsack
 }
 printf("Discrete Knapsack profit = %f\n",profit);
-printf("Continuous Knapsack also includes item %d with portion: %f\n", k,
-(float)m)/w[k]);
-profit = profit + ((float)m)/w[k] * p[k];
+printf("Continuous Knapsack also includes item %d with portion: %f\n",k,(float)m/w[k]);
+profit=profit+((float)m)/w[k]*p[k];
 printf("Continuous Knapsack profit = %f\n",profit);
 }
 int main()
@@ -39,7 +38,7 @@ int main()
 int i;
 printf("Enter the no. of items: ");
 scanf("%d",&n);
-printf("Enter the weights of n items: ");
+printf("Enter the weights of n items:");
 for(i=0;i<n;i++)
 scanf("%d",&w[i]);
 printf("Enter the prices of n items: ");
