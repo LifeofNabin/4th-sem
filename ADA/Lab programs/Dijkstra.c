d2c3e1ea9f6fe2ac91347a6dbf62d17ec74fe691
Nabin <70052993+LifeofNabin@users.noreply.github.com>
2024-07-30 18:19:57 +0900

    Dijkstra.c

--- ADA/Lab programs/Dijkstra.c
+++ ADA/Lab programs/Dijkstra.c
@@ -1,5 +1,6 @@
 #include<stdio.h>
 int cost[10][10],n,dist[10];
+int i,v,j;
 int minm(int m, int n)
 {
 return(( m < n) ? m: n);
@@ -8,17 +9,17 @@ void dijkstra(int source)
 {
 int s[10]={0};
 int min, w=0;
-for(int i=0;i<n;i++)
+for(i=0;i<n;i++)
 dist[i]=cost[source][i];
 //Initialize dist from source to source as 0
 dist[source] = 0;
 //mark source vertex - estimated for its shortest path
 s[source] = 1;
-for(int i=0; i < n-1; i++)
+for(i=0; i < n-1; i++)
 {
 //Find the nearest neighbour vertex
 min = 999;
-for(int j = 0; j < n; j++)
+for(j = 0; j < n; j++)
 {
 if ((s[j] == 0 ) && (min > dist[j]))
 {
@@ -28,7 +29,7 @@ w = j;
 }
 s[w]=1;
 //Update the shortest path of neighbour of w
-for(int v=0;v<n;v++)
+for(v=0;v<n;v++)
 {
 if(s[v]==0 && cost[w][v]!=999)
 {
@@ -43,13 +44,13 @@ int source;
 printf("Enter the no.of vertices:");
 scanf("%d",&n);
 printf("Enter the cost matrix\n");
-for(int i=0;i<n;i++)
-for(int j=0;j<n;j++)
+for(i=0;i<n;i++)
+for(j=0;j<n;j++)
 scanf("%d",&cost[i][j]);
 printf("Enter the source vertex:");
 scanf("%d",&source);
 dijkstra(source);
 printf("the shortest distance is...");
-for(int i=0; i<n; i++)
+for(i=0; i<n; i++)
 printf("Cost from %d to %d is %d\n",source,i,dist[i]);
 }
